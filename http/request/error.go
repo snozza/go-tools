0309264e900f17d202103e75e2b91b12bacef40d
Andrew <andrewcsnead@gmail.com>
2018-01-07 22:44:15 +0000

    add basic http error

---
+++ http/request/error.go
@@ -0,0 +1,34 @@
+package request
+
+import "net/http"
+
+// Error is an HTTP error.
+type Error int
+
+// Error implementation.
+func (e Error) Error() string {
+	return http.StatusText(int(e))
+}
+
+// IsStatus returns true if err is a status code
+func IsStatus(err error, code int) bool {
+	e, ok := err.(Error)
+	return ok && int(e) == code
+}
+
+// IsClient returns true if err represents a 4xx error.
+func IsClient(err error) bool {
+	e, ok := err.(Error)
+	return ok && e >= 400 && e < 500
+}
+
+// IsServer returns true if err represents a 5xx error.
+func IsServer(err error) bool {
+	e, ok := err.(Error)
+	return ok && e >= 500
+}
+
+// IsNotFound returns true if erris a 404.
+func IsNotFound(err error) bool {
+	return IsStatus(err, 404)
+}
---
+++ http/request/error_test.go
@@ -0,0 +1,33 @@
+package request
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestError(t *testing.T) {
+	assert.Equal(t, "Not Found", Error(404).Error())
+	assert.Equal(t, "Bad Request", Error(400).Error())
+}
+
+func TestIsNotFound(t *testing.T) {
+	assert.True(t, IsNotFound(Error(404)))
+	assert.False(t, IsNotFound(Error(500)))
+}
+
+func TestIsStatus(t *testing.T) {
+	assert.True(t, IsStatus(Error(404), 404))
+	assert.True(t, IsStatus(Error(500), 500))
+	assert.False(t, IsStatus(Error(500), 400))
+}
+
+func TestIsClient(t *testing.T) {
+	assert.True(t, IsClient(Error(400)))
+	assert.False(t, IsClient(Error(500)))
+}
+
+func TestIsServer(t *testing.T) {
+	assert.True(t, IsServer(Error(500)))
+	assert.False(t, IsServer(Error(400)))
+}
