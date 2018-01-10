17965afe260fcf44e989585c7ddaa28a69e6b3ad
Andrew <andrewcsnead@gmail.com>
2018-01-10 22:20:12 +0000

    add optional res code for xml

--- http/response/xml.go
+++ http/response/xml.go
@@ -8,13 +8,23 @@ import (
 // XML response with optional status Code
 func XML(w http.ResponseWriter, val interface{}, code ...int) {
 	var b []byte
-	//var err error
+	var err error
 
 	if Pretty {
-		b, _ = xml.MarshalIndent(val, "", " ")
+		b, err = xml.MarshalIndent(val, "", " ")
+	} else {
+		b, err = xml.Marshal(val)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(code) > 0 {
+		w.WriteHeader(code[0])
 	}
 
 	w.Header().Set("Content-Type", "application/xml")
-
 	w.Write(b)
 }
--- http/response/xml_test.go
+++ http/response/xml_test.go
@@ -12,6 +12,29 @@ func TestXMLPretty(t *testing.T) {
 	res := httptest.NewRecorder()
 	XML(res, &User{"Tobi", "Ferret"})
 	assert.Equal(t, 200, res.Code)
-	assert.Equal(t, "<User>\n  <First>Tobi</First>\n  <Last>Ferret</Last>\n</User>", string(res.Body.Bytes()))
+	assert.Equal(t, "<User>\n <First>Tobi</First>\n <Last>Ferret</Last>\n</User>", string(res.Body.Bytes()))
 	assert.Equal(t, "application/xml", res.HeaderMap["Content-Type"][0])
+}
+
+func TestXML(t *testing.T) {
+	Pretty = false
+	res := httptest.NewRecorder()
+	XML(res, &User{"Tobi", "Ferret"})
+	assert.Equal(t, 200, res.Code)
+	assert.Equal(t, `<User><First>Tobi</First><Last>Ferret</Last></User>`, string(res.Body.Bytes()))
+	assert.Equal(t, "application/xml", res.HeaderMap["Content-Type"][0])
+}
+
+func TestXMLError(t *testing.T) {
+	res := httptest.NewRecorder()
+	invalidBody := make(chan int, 0)
+	XML(res, invalidBody)
+	assert.Equal(t, 500, res.Code)
+	assert.Equal(t, "text/plain; charset=utf-8", res.HeaderMap["Content-Type"][0])
+}
+
+func TestXMLCode(t *testing.T) {
+	res := httptest.NewRecorder()
+	XML(res, &User{}, 202)
+	assert.Equal(t, 202, res.Code)
 }
